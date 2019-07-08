2e2237a1181ae1be4c865c6b8f9e15c4893487ae
hsrsbd <hsraffety@gmail.com>
2019-07-08 14:15:02 -0700

    retrieving specific user.

--- index.js
+++ index.js
@@ -9,11 +9,27 @@ const port = 5000;
 
 // Create a user
 server.post('/api/users', (req, res) => {
+  const { name, bio } = req.body;
   const userInfo = req.body;
-  console.log(userInfo);
-  db.insert()
-    .then()
-    .catch();
+
+  console.log(`Name: ${name}, Bio: ${bio}`);
+
+  if (name && bio) {
+    db.insert(userInfo)
+      .then(user => {
+        console.log(user);
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: 'There was an error while saving the user to the database'
+        });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: 'Please provide a name and biography for the user.'
+    });
+  }
 });
 
 // Read all users
@@ -22,16 +38,34 @@ server.get('/api/users', (req, res) => {
     .then(users => {
       res.status(200).json(users);
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: 'There was an error while retrieving the users' });
     });
 });
 
 // Read specific user
 server.get('/api/users/:id', (req, res) => {
-  db.findById()
-    .then()
-    .catch();
+  const id = req.params.id; // specific user id
+  db.findById(id)
+    .then(user => {
+      if (user) {
+        // if we have the user pulled in through dynamic id
+        res.status(200).json(user);
+      } else {
+        // dude ain't there
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      // something goes wrong (api err from what's being thrown at it?)
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
 });
 
 // Update a user
