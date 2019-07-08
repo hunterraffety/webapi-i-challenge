d3737554784d3b065aaa984a9366d17afc0e5ded
hsrsbd <hsraffety@gmail.com>
2019-07-08 12:18:07 -0700

    writing api endpoints. read all users.

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// implement your API here
+// Dependencies
+const express = require('express');
+
+// Express config.
+const db = require('./data/db');
+const server = express();
+server.use(express.json());
+const port = 5000;
+
+// Create a user
+server.post('/api/users', (req, res) => {
+  db.find()
+    .then()
+    .catch();
+});
+
+// Read all users
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// Read specific user
+server.get('/api/users/:id', (req, res) => {
+  db.find()
+    .then()
+    .catch();
+});
+
+// Update a user
+server.put('/api/users', (req, res) => {
+  db.update()
+    .then()
+    .catch();
+});
+
+// Delete a user
+server.delete('/api/users', (req, res) => {
+  db.remove()
+    .then()
+    .catch();
+});
+
+// Initialize the server
+server.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
